e2022bb554aac37231ff2432177a8391dd9d9731
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-05-07 13:31:27 +0200

    Add files via upload

---
+++ links/lab1/greet.py
@@ -0,0 +1,9 @@
+# greet.py
+def main():
+    name = input("What is your name? ")
+    print(f"\n🎉 Hello, {name}!")
+    print("Welcome to Git Basics & Tooling!")
+    print("You just ran a file that is tracked by Git!")
+
+if __name__ == "__main__":
+    main()
